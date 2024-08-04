91d9a0727daa4078d23f3082867da320c33db653
imjustmaxie <78627715+imjustmaxie@users.noreply.github.com>
2024-08-04 20:15:29 +0800

    Update Firefly.js

--- Firefly.js
+++ Firefly.js
@@ -13,13 +13,13 @@ Update Interval: 5
 let body = $response.body;
 body = JSON.parse(body);
 
-body.credits["firefly_free_credit"].quota.available = 99999;
-body.credits["firefly_free_credit"].quota.total = 99999;
+body.credits["firefly_free_credit"].quota.available = 999;
+body.credits["firefly_free_credit"].quota.total = 999;
 body.credits["firefly_free_credit"].quota.used = 0;
 
-body.total.availableUntil = "2100-01-01T00:00:01.000Z";
-body.total.quota.available = 99999;
-body.total.quota.total = 99999;
+body.total.availableUntil = "2100-01-01T01:00:00.000Z";
+body.total.quota.available = 999;
+body.total.quota.total = 999;
 body.total.quota.used = 0;
 
 body = JSON.stringify(body)
