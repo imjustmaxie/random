620ae9106eb74f48dcd64720a0ad927346981148
imjustmaxie <78627715+imjustmaxie@users.noreply.github.com>
2024-08-04 18:18:38 +0800

    Update LightroomV1.js

--- LightroomV1.js
+++ LightroomV1.js
@@ -1,3 +1,15 @@
+/*
+
+Hostname: lcs-mobile-cops.adobe.io
+
+Script Type: Response
+Matching URL: https://lcs-mobile.cops.adobe.io/mobiles/access_profile/v3
+
+Body Required: True
+Update Interval: 5
+
+ */
+
 let body = $response.body;
 body = JSON.parse(body);
 
@@ -6,13 +18,14 @@ a = JSON.parse(a);
 
 a.responseType = "INITIAL";
 a.profileStatus = "PROFILE_AVAILABLE";
-a.profileStatusReason = 1001;
-a.profileStatusReasonText = "Profile Available due to a temporary entitlement created to hide provisioning delays (HADES)";
+a.profileStatusReason = 1000;
+a.profileStatusReasonText = "Profile Available due to an acquired plan provisioned and ACTIVE";
 
-a.appProfile.accessibleItems[0].source.id = "TEMP_LICENSE_PROD";
-a.appProfile.accessibleItems[0].source["type"] = "TEMPORARY_ENTITLEMENT";
+//a.appProfile.accessibleItems[0].source.id = "TEMP_LICENSE_PROD";
+a.appProfile.appId = "PSXIOS1";
+a.appProfile.accessibleItems[0].source["type"] = "LICENSE";
 a.appProfile.accessibleItems[0].source["status_reason"] = "NORMAL";
-a.appProfile.accessibleItems[0].source["can_access_until"] = 0;
+a.appProfile.accessibleItems[0].source["can_access_until"] = 4102395009000; // 12/31/2099 06:10:09 PM
 
 a.appProfile.accessibleItems[0]["fulfillable_items"] = {
           "cc_storage": {
@@ -44,6 +57,14 @@ a.appProfile.accessibleItems[0]["fulfillable_items"] = {
               "rollover": 0
             }
           },
+          "photoshop_express": {
+            "enabled": true,
+            "charging_model": {
+              "model": "RECURRING",
+              "overage": "NA",
+              "rollover": 0
+            }
+          },
           "core_services_cc": {
             "enabled": true,
             "feature_sets": {
@@ -61,17 +82,19 @@ a.appProfile.accessibleItems[0]["fulfillable_items"] = {
           }
       };
 
-a.appProfile.accessibleItems[1] = a.appProfile.accessibleItems[0]
+//a.appProfile.accessibleItems[1] = a.appProfile.accessibleItems[0]
 
-a.controlProfile.creationTimeStamp = 1722658065167;
-a.controlProfile.cacheLifetime = 15638384677;
-a.controlProfile.validUptoTimestamp = 1722744449844;
+//a.controlProfile.creationTimeStamp = 1722658065167;
+a.controlProfile.nglAppId = "PSXIOS1";
+a.controlProfile.requesterNglAppId = "PSXIOS1";
+a.controlProfile.cacheLifetime = 39970872755;
+a.controlProfile.validUptoTimestamp = 4102395009000;
 a.controlProfile.cacheRefreshControl = { appRefreshInterval: 87840000, nglLibRefreshInterval: 87840000 };
 a.controlProfile.overrideStatusForLocalSignout = "PROFILE_DENIED";
 a.controlProfile.appUsageTrackingControl.enabledTracking = false;
 
 a.frlProfile = "{}";
-a.legacyProfile = '{"licenseId":"TEMP_LICENSE_PROD","licenseType":3,"licenseVersion":"1.0","effectiveEndTimestamp":1722744449844,"graceTime":0,"licensedFeatures":[],"enigmaData":{"productId":598,"serialKey":"719594172484074650698776","clearSerialKey":"90970938077827684226","locale":"ALL","associatedLocales":"ALL","platform":0,"isk":5984027,"customerId":0,"deliveryMethod":3,"pc":true,"rb":true}}';
+a.legacyProfile = '{"licenseId":"TEMP_LICENSE_PROD","licenseType":3,"licenseVersion":"1.0","effectiveEndTimestamp":4102395009000,"graceTime":0,"licensedFeatures":[],"enigmaData":{"productId":598,"serialKey":"994304170022856772300296","clearSerialKey":"12044938704595791057","locale":"ALL","associatedLocales":"ALL","platform":0,"isk":5984027,"customerId":0,"deliveryMethod":3,"pc":true,"rb":false}}';
 a.additionalLegacyProfiles = '{}';
 
 body.asnp.payload = btoa(JSON.stringify(a))
