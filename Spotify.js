93f9c752735c86dd63ac8b4bf7361a363f913bdb
imjustmaxie <78627715+imjustmaxie@users.noreply.github.com>
2024-08-07 14:18:57 +0800

    Update Spotify.js

--- Spotify.js
+++ Spotify.js
@@ -7,7 +7,7 @@ Matching URL : ^https:\/\/gae(.+)\.com\/device-capabilities\/v\d\/capabilities(.
 let objet = $response.body;
 
 objet = JSON.parse(objet);
-objet.license = "premium";
+objet.license = "premium_hifi";
 objet.supports_hifi.fully_supported = true;
 objet.supports_hifi.user_eligible = true;
 objet.supports_hifi.device_supported = true;
