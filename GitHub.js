f8ac88f0085b9a551e766dde8a391248f1636ddc
imjustmaxie <78627715+imjustmaxie@users.noreply.github.com>
2024-08-07 22:29:02 +0800

    Update GitHub.js

--- GitHub.js
+++ GitHub.js
@@ -5,17 +5,15 @@ Matching URL: https://api.github.com/graphql
 
 */
 
-let hdr = $request.body
-hdr = JSON.parse(hdr)
-if hdr.operationName != "Authorizations" {
-  $done({$response.body});
-}
+let objet = $response.body;
+objet = JSON.parse(objet)
+if typeof (objet.data.viewer.isEmployee) == "undefined" {
+  }
 else {
-  let objet = $response.body;
-  objet = JSON.parse(objet);
   objet.data.viewer.isProPlan = true;
   objet.data.viewer.hasAppleIapSubscription = true;
   objet.data.viewer.isEmployee = true;
-  let b = JSON.stringify(objet);
-  $done({b});
 }
+let b = JSON.stringify(objet);
+$done({b});
+
