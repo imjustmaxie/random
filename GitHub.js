ef36d0a00c159829afea5dee72f6727a8b47d799
imjustmaxie <78627715+imjustmaxie@users.noreply.github.com>
2024-08-07 22:41:24 +0800

    Update GitHub.js

--- GitHub.js
+++ GitHub.js
@@ -6,7 +6,7 @@ Matching URL: https://api.github.com/graphql
 */
 
 let obj = JSON.parse($response.body);
-if (obj.data && obj.data.viewer) {
+if (obj.data && obj.data.viewer && obj.data.viewerHasTradeRestrictions) {
   obj.data.viewer.isProPlan = true;
   obj.data.viewer.isEmployee = true;
   obj.data.viewer.hasAppleIapSubscription = true;
