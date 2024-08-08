5315ff8d05fe1128b2323bc6e82f4ee61c6b565b
imjustmaxie <78627715+imjustmaxie@users.noreply.github.com>
2024-08-08 12:08:57 +0800

    Update GitHub.js

--- GitHub.js
+++ GitHub.js
@@ -5,8 +5,12 @@ Matching URL: https://api.github.com/graphql
 
 */
 
-let objet = JSON.parse($response.body);
-if (typeof(objet.data.viewerHasTradeRestrictions)!== "undefined") {
+let hdr = JSON.parse($response.headers);
+if (hdr["x-apollo-operation-name"] == "Authorizations")
+{
+  let objet = JSON.parse($response.body);
+  // if (typeof(objet.data.viewerHasTradeRestrictions)!== "undefined") 
+
   objet.data.viewer.isProPlan = true;
   objet.data.viewer.isEmployee = true;
   objet.data.viewer.hasAppleIapSubscription = true;
